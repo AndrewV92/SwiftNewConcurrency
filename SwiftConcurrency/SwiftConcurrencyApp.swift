52c437552329c20abbb92ecac413392afa8dac7f
AndrewV92 <vorobyevandrew@gmail.com>
2022-07-01 22:28:41 +0300

    initial commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Андрей Воробьев on 01.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
